60913c722a28a58ee232573b5c2e8180f8732fa2
Alexander Lyashuk <mooskagh@gmail.com>
2018-02-16 22:21:36 +0100

    Show edit buttons to everyone.

--- moder/actions/comp_actions.py
+++ moder/actions/comp_actions.py
@@ -30,34 +30,30 @@ class CompetitionAdminzAction(CompetitionAction):
 @RegisterAction
 class CompetitionEditorLink(CompetitionAction):
     TITLE = 'Править текст'
+    PERM = '@auth'
 
     def GetUrl(self):
         return reverse("edit_compdoc", args=(self.obj.id, ))
 
     @classmethod
     def IsAllowed(cls, request, object):
-        if request.perm(cls.PERM):
-            return True
-
-        obj = cls.EnsureObj(object)
-        if obj and obj.competition and obj.competition.owner == request.user:
-            return True
-        return False
+        if obj and obj.competition and obj.competition.owner:
+            return obj.competition.owner == request.user
+        else:
+            return request.perm(cls.PERM)
 
 
 @RegisterAction
 class CompetitionEditorLink(CompetitionAction):
     TITLE = 'Править событие'
+    PERM = '@auth'
 
     def GetUrl(self):
         return reverse("edit_competition", args=(self.obj.competition.id, ))
 
     @classmethod
     def IsAllowed(cls, request, object):
-        if request.perm(cls.PERM):
-            return True
-
-        obj = cls.EnsureObj(object)
-        if obj and obj.competition and obj.competition.owner == request.user:
-            return True
-        return False
+        if obj and obj.competition and obj.competition.owner:
+            return obj.competition.owner == request.user
+        else:
+            return request.perm(cls.PERM)
