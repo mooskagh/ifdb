adbe41c685db76577ce2f47003f543a305d042cf
Alexander Lyashuk <mooskagh@gmail.com>
2019-07-28 21:29:22 +0200

    Support comment likes.

--- games/static/game-details.js
+++ games/static/game-details.js
@@ -1,5 +1,22 @@
 $(function() {
   'use strict';
+
+  function GetCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = jQuery.trim(cookies[i]);
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) === (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  }
+
   $('#reply-1 textarea').focus(function() {
     $('#reply-2').hide();
     $('#reply-1 input').show();
@@ -24,9 +41,28 @@ $(function() {
     return false;
   });
 
-  $('.start-lunchator').click(function() {
-    $(this).hide();
-    $('.loonchator-fail').show();
+  function setUpLikes(parent) {
+    $(parent).find('a').click(function(e) {
+      var id = $(this).closest('.comment-box').attr('data-id');
+      $.ajax({
+        type: 'POST',
+        url: '/json/commentvote/',
+        data: {
+          'comment': id,
+          'cur_val': $(this).attr('like-value'),
+          'csrfmiddlewaretoken': GetCookie('csrftoken')
+        },
+        success: function(data) {
+          $(parent).html(data);
+          setUpLikes(parent);
+        }
+      });
+      return false;
+    });
+  }
+
+  $('.comment-box .likes').each(function() {
+    setUpLikes(this);
   });
 
   if ($.fn.slick !== undefined) {
