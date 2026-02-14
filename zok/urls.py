89e712da1eb085c0e4c78389d16e8b063be5e518
Alexander Lyashuk <mooskagh@gmail.com>
2026-02-14 15:36:28 +0100

    Accept optional trailing slash in zok competition URLs

--- zok/urls.py
+++ zok/urls.py
@@ -11,13 +11,13 @@ urlpatterns = [
     re_path(r"^", include("games.urls"), name="games"),
     re_path(r"^", include("moder.urls"), name="moder"),
     re_path(
-        r"^2025/(?P<doc>.*)$",
+        r"^2025/(?P<doc>.*?)/?$",
         show_competition,
         {"slug": "zok-2025"},
         name="show_competition",
     ),
     re_path(
-        r"^(?P<doc>.*)$",
+        r"^(?P<doc>.*?)/?$",
         show_competition,
         {"slug": "zok-2026"},
         name="show_competition",
