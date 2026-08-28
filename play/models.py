86cca2847620658d8333972e0b309d6d3ff4272d
Alexander Lyashuk <mooskagh@gmail.com>
2026-08-28 21:25:09 +0200

    Remove play templates

--- play/models.py
+++ play/models.py
@@ -1,24 +1,13 @@
 from django.db import models
 
 
-class Template(models.Model):
-    class Meta:
-        default_permissions = ()
-
-    slug = models.SlugField(unique=True)
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
 class Playable(models.Model):
     class Meta:
         default_permissions = ()
 
     slug = models.SlugField(unique=True)
     game = models.ForeignKey("games.Game", on_delete=models.PROTECT)
-    template = models.ForeignKey(Template, on_delete=models.PROTECT)
+    template = models.SlugField()
     config = models.JSONField(default=dict, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
--- play/tests.py
+++ play/tests.py
@@ -1,29 +1,29 @@
+from django.db import models
 from django.test import TestCase
 from django.utils.timezone import now
 
 from games.models import Game
 
-from .models import Playable, Template
+from .models import Playable
 
 
 class ModelTests(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.game = Game.objects.create(title="A game", creation_time=now())
-        cls.template = Template.objects.create(slug="instead", name="Instead")
-
-    def test_template(self):
-        self.assertEqual(str(self.template), "Instead")
-        self.assertEqual(Template._meta.default_permissions, ())
 
     def test_playable(self):
         playable = Playable.objects.create(
             slug="demo",
             game=self.game,
-            template=self.template,
+            template="instead",
         )
 
         self.assertEqual(str(playable), "demo")
+        self.assertEqual(playable.template, "instead")
+        self.assertIsInstance(
+            Playable._meta.get_field("template"), models.SlugField
+        )
         self.assertEqual(playable.config, {})
         self.assertIsNotNone(playable.created)
         self.assertIsNotNone(playable.updated)
@@ -33,12 +33,12 @@ class ModelTests(TestCase):
         first = Playable.objects.create(
             slug="first",
             game=self.game,
-            template=self.template,
+            template="instead",
         )
         second = Playable.objects.create(
             slug="second",
             game=self.game,
-            template=self.template,
+            template="instead",
         )
 
         first.config["theme"] = "dark"
